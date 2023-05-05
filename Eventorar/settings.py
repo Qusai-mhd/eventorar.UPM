5dd6b5b711cb67219a1b3bd220fc0f0700112588
OmarRa15 <omarhoms0@gmail.com>
2023-05-05 19:27:16 +0300

    Added necessary files for azure deployment.

--- Eventorar/settings.py
+++ Eventorar/settings.py
@@ -150,6 +150,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'event/static/'),
